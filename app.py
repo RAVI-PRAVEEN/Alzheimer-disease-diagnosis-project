20c1054a880ee6c0eb489e2966528b05e79b8880
MAMIDI RAVI PRAVEEN <121561557+RAVI-PRAVEEN@users.noreply.github.com>
2025-05-13 18:37:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -69,4 +69,6 @@ def predict():
     return render_template('result.html', image_url=filepath, prediction=prediction)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
