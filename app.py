e5cf1394fedb02e10bf4380e92c3c41e8bbefcb0
RAVI-PRAVEEN <ravipraveenmamidi126@gmail.com>
2025-05-13 22:17:15 +0530

    second

--- app.py
+++ app.py
@@ -69,6 +69,4 @@ def predict():
     return render_template('result.html', image_url=filepath, prediction=prediction)
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
-
+    app.run(debug=True)
